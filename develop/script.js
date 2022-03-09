194e6d07b7cc37c53f050353f84c1ec5783dcfe8
Nibbler <kwimbish@hotmail.com>
2022-03-09 14:38:30 -0600

    trying to fix background colors

--- develop/script.js
+++ develop/script.js
@@ -1,14 +1,28 @@
-// Function that gets the most current date
+var date = "";
+var dateTime = "";
+var hour = "";
+
+//Function that gets the current date
 var currentTime = function(){
     date = moment(new Date());
     dateTime.html(date.format('MMMM Do YYYY, h:mm:ss a'));
 }
-// Function that  maintains accurate date and time in the heasder
+
+// Function that keeps accurate date/time in the header
 $(document).ready(function(){
     dateTime = $("#currentDay");
     currentTime();
     setInterval(currentTime, 1000);
 });
 
-currentTime();
-
+var currentHour = (new Date()).getHours();
+$('row').each(function(){
+    var val = parseInt($(this).prop('id'));
+    if(val > currentHour && val < currentHour + 6){
+      $(this).css('background-color', 'green');      
+    }else if(val < currentHour && val > currentHour - 6){
+      $(this).css('background-color', 'gray');
+    }else if(val === currentHour){
+      $(this).css('background-color', 'red');
+    }
+});
