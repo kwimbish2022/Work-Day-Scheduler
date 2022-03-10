370f428b42243189d4ea2d10fab69a9f867acbf5
Nibbler <kwimbish@hotmail.com>
2022-03-10 15:01:36 -0600

    initial localstorage functions

--- develop/script.js
+++ develop/script.js
@@ -15,14 +15,41 @@ $(document).ready(function(){
     setInterval(currentTime, 1000);
 });
 
-var currentHour = (new Date()).getHours();
-$('row').each(function(){
-    var val = parseInt($(this).prop('id'));
-    if(val > currentHour && val < currentHour + 6){
-      $(this).css('background-color', 'green');      
-    }else if(val < currentHour && val > currentHour - 6){
-      $(this).css('background-color', 'gray');
-    }else if(val === currentHour){
-      $(this).css('background-color', 'red');
-    }
-});
+// To change background colors based on hour
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('H'));
+
+
+Array.from(rows).forEach(row => {
+  var rowIdString = parseInt(row.id);
+  if (currentHour === rowIdString) {
+    setColor(row, "red");
+  } else if (currentHour < rowIdString) {
+    setColor(row, "green");
+  } else { 
+    setColor(row, "grey");
+  }
+}); 
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
+
+// add event listener to save buttons
+var saveBtn = document.querySelectorAll('.saveBtn');
+for (var i = 0; i < saveBtn.length; i++) {
+  saveBtn[i].addEventListener("click", saveInfo);
+}
+
+// save to local storage
+function saveInfo() {
+  var textInput = this.parentElement.value;
+  var key = this.parentElement.getAttribute('id');
+  localStorage.getItem(key, textInput);
+  console.log(saveInfo);
+}
+
+for (var i =9; i < 18; i++) {
+  var textVal = localStorage.getItem(i);
+  document.getElementById(i).value = textVal;
+}
